43a7c2a47256d3906f80b8bcd001635b9aef36aa
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-03-11 13:28:04 +0100

    simple send message

--- js/widget.js
+++ js/widget.js
@@ -1,17 +1,14 @@
 import "./widget.css";
 
 function render({ model, el }) {
-	let btn = document.createElement("button");
-	btn.classList.add("anywidget_instructions-counter-button");
-	btn.innerHTML = `count is ${model.get("value")}`;
-	btn.addEventListener("click", () => {
-		model.set("value", model.get("value") + 1);
-		model.save_changes();
-	});
-	model.on("change:value", () => {
-		btn.innerHTML = `count is ${model.get("value")}`;
-	});
-	el.appendChild(btn);
+  let btn = document.createElement("button");
+  btn.classList.add("anywidget_instructions-counter-button");
+  el.appendChild(btn);
+
+  
+  model.on("msg:custom", (msg) => {
+    btn.innerHTML = `Message is ${msg}`;
+  });
 }
 
 export default { render };
